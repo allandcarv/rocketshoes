c21c06e57623e99b12488e47fafacb26f3862e1d
Allan Carvalho <allandcarv@gmail.com>
2019-10-04 11:46:36 -0300

    Adicionando ao Carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
+import PropTypes from 'prop-types';
 
 import { ProductList } from './styles';
 
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
-export default class Main extends Component {
+class Main extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  };
+
   state = {
     products: [],
   };
@@ -22,6 +28,15 @@ export default class Main extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -33,7 +48,10 @@ export default class Main extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -46,3 +64,5 @@ export default class Main extends Component {
     );
   }
 }
+
+export default connect()(Main);
